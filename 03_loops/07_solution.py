9979120fc43db85ee8a9f349dbe0832da277e754
Mohit Saud <mohit@prabidhilabs.com>
2024-06-08 12:09:00 +0545

    Add loops folder

---
+++ 03_loops/07_solution.py
@@ -0,0 +1,7 @@
+while True:
+    number = int(input("Enter value b/w 1 and 10: "))
+    if 1>= number <=10:
+        print("Thanks")
+        break
+    else:
+        print("Please provide number between 1 and 10")
